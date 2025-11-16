69f2dc3e55a37911ea807557808e83e092a65df3
Devansh Sood <sooddevansh143.gmail.com>
2025-11-17 01:18:30 +0530

    images bug fixed

--- script.js
+++ script.js
@@ -1,99 +1,132 @@
-let s1=document.querySelector('#s1');
-let s2=document.querySelector('#s2');
-let h5=document.querySelector('h5');
+let s1 = document.querySelector('#s1');
+let s2 = document.querySelector('#s2');
+let h3 = document.querySelector('h3');
 
-let left=document.querySelector('.left');
-let right=document.querySelector('.right');
-let btn=document.querySelector('.btn');
+let left = document.querySelector('.left');
+let right = document.querySelector('.right');
+let btn = document.querySelector('.btn');
 
-
-let arr=["rock","paper","scissor"];
-
-let image1=document.querySelector('#image1');
-let image2=document.querySelector('#image2');
-
-
-let images=["car.png","p.png","generated.png"];
-
-console.log(images);
-
-
-function playgame(){
-    
-    let img1=Math.floor(Math.random()*arr.length);
-let img2=Math.floor(Math.random()*arr.length);
-
-let player1=arr[img1];
-// console.log(player1);
-
-let player2=arr[img2];
-// console.log(player2);
+let image1 = document.querySelector('#image1');
+let image2 = document.querySelector('#image2');
 
 
 
-// condition when both plays same 
-if(player1===player2) {
-   
-      h5.textContent="Draw game";
-      right.style.backgroundColor='pink';
-     left.style.backgroundColor='pink';
-     
-     image1.src=`${images[player1]}`;
-      image2.src=`${images[player2]}`;
-
-}
 
 
-// conditions for player2 wins
-else if(player1===arr[0] && player2===arr[1]){
+
+// making the arrays for inputs and images to show respectively
+
+
+let arr = ["Rock", "Paper", "Scissor"];
+let images = ["./assets/rock.png", "./assets/paper.png", "./assets/scissor.png"];
+
+
+//creating a function which does all the calculation
+
+
+function playgame() {
+
+
+    // random1 and random2 stores the values for both the right and left divs -- values like index nos of---  arr array.
+
+
+    let random1 = Math.floor(Math.random() * arr.length);
+    let random2 = Math.floor(Math.random() * arr.length);
+
+
+    // now accessing the index of arr array through the values stored in random1 and random2 and storing them in player1 and player2
+    let player1 = arr[random1];
+    let player2 = arr[random2];
   
-    h5.textContent='player 2 wins';
-    right.style.backgroundColor='green';
-     left.style.backgroundColor='red';
-      image1.src=`${images[player1]}`;
-      image2.src=`${images[player2]}`;
 
-}
-else if(player1===arr[1] && player2===arr[2]){
- 
-    h5.textContent='player 2 wins';
-right.style.backgroundColor='green';
-     left.style.backgroundColor='red';
-      image1.src=`${images[player1]}`;
-      image2.src=`${images[player2]}`;
+
+
+    // condition when both plays same input
+
+
+
+    if (player1 === player2) {
+
+        h3.textContent = "Draw game";
+
+        right.style.backgroundColor = '#7610bfff';
+        left.style.backgroundColor = '#7610bfff';
+
+
+    }
+
+
+    // conditions for player2 winning
+
+    else if (player1 === arr[0] && player2 === arr[1]) {
+
+        h3.textContent = 'player 2 wins';
+        right.style.backgroundColor = 'green';
+        left.style.backgroundColor = 'red';
+
+    }
+
+
+
+    else if (player1 === arr[1] && player2 === arr[2]) {
+
+        h3.textContent = 'player 2 wins';
+        right.style.backgroundColor = 'green';
+        left.style.backgroundColor = 'red';
+
+ }
+
+
+
+    else if (player1 === arr[2] && player2 === arr[0]) {
+
+        h3.textContent = 'player 2 wins';
+        right.style.backgroundColor = 'green';
+        left.style.backgroundColor = 'red';
+
+
+    }
+
+
+
+    // condition for player 1 winnings
+
+
+    else {
+
+        h3.textContent = 'player 1 wins';
+        right.style.backgroundColor = 'red';
+        left.style.backgroundColor = 'green';
+
+    }
+
+
+
+    // using.src method to print the respective images to the respective inputs 
+
     
-}
+    image1.src = `${images[random1]}`;
+    image2.src = `${images[random2]}`;
 
-else if(player1===arr[2] && player2===arr[0]){
 
-  h5.textContent='player 2 wins';
-    right.style.backgroundColor='green';
-     left.style.backgroundColor='red';
-      image1.src=`${images[player1]}`;
-      image2.src=`${images[player2]}`;
-    
-}
 
-// condition where player 1 winsx
-else{
+    // now putting the inputs in the span tags
 
-    h5.textContent='player 1 wins';
-    right.style.backgroundColor='red';
-     left.style.backgroundColor='green';
-      image1.src=`${images[player1]}`;
-      image2.src=`${images[player2]}`;
-}
- s1.textContent=`${player1}`;
-     s2.textContent=`${player2}`;
-};
+
+    s1.textContent = `${player1}`;
+    s2.textContent = `${player2}`;
+
+   
+};  // function  playgame ends here....
 
 
 
 // button movement 
 
-btn.addEventListener('click',function(){
+btn.addEventListener('click', function () {
 
     playgame();
 
-
 });
+
+
